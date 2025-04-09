22e0d5f63767e4bf8db90b162aa30ff9e8138320
dan-wamithi <danielwainaina588@gmail.com>
2025-04-09 19:26:41 +0300

    Creating table for book and book_authprs

--- bookstore.sql
+++ bookstore.sql
@@ -1,1 +1,21 @@
+
+--create database bookstore;
 create database bookstore;
+
+-- create table for books with the following fields:
+CREATE TABLE book (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    publication_date DATE,
+    price DECIMAL(10, 2) NOT NULL,
+    publisher_id INT,
+    language_id INT
+    );
+
+    -- create table for authors with the following fields:
+
+CREATE TABLE book_author (
+    book_id INT primary key,
+    authorName Varchar(50)
+    );
+
